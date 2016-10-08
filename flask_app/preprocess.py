c0589f5d878169aab544a42f41962ff96a125a79
Pankaj Bhambhani <pankajb64@gmail.com>
2016-10-08 19:58:05 -0400

    Updated Pre-Process script to handle rotated images

--- flask_app/preprocess.py
+++ flask_app/preprocess.py
@@ -2,7 +2,14 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('board.jpg')
+def rotateImage(image, angle):
+   (h, w) = image.shape[:2]
+   center = (w / 2, h / 2)
+   M = cv2.getRotationMatrix2D(center,angle,1.0)
+   rotated_image = cv2.warpAffine(image, M, (w,h))
+   return rotated_image
+
+img = cv2.imread('board5.jpeg')
 res = cv2.resize(img, None, fx=0.2, fy=0.2, interpolation=cv2.INTER_CUBIC)
 gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
 (thresh, im_bw) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
@@ -14,16 +21,36 @@ cnt = contours[2]
 epsilon = 0.1*cv2.arcLength(cnt,True)
 approx = cv2.approxPolyDP(cnt,epsilon,True)
 
-x1 = approx[0][0][0]
-y1 = approx[0][0][1]
-x2 = approx[2][0][0]
-y2 = approx[2][0][1]
+print cnt.shape
+#x,y,w,h = cv2.boundingRect(cnt)
+#x1 = approx[0][0][0]
+#y1 = approx[0][0][1]
+#x2 = approx[2][0][0]
+#y2 = approx[2][0][1]
 
-#cv2.rectangle(t, (x1,y1), (x2, y2), (0,255,0), thickness=5);
+rect = cv2.minAreaRect(cnt)
+angle = rect[2]
+box = cv2.cv.BoxPoints(rect)
+box = np.int0(box)
+#print box
+#cv2.drawContours(t,[box],0,(0,0,255),2)
+#print box.shape
 
-roi = t[min(y1,y2):max(y1,y2), min(x1,x2):max(x1,x2)]
+x1 = box[0][0]
+y1 = box[0][1]-20
+x2 = box[2][0]
+y2 = box[2][1]+20
 
-cv2.imshow('image', roi)
+rot_t = rotateImage(t,angle)
+rot_c = rot_t[min(y1,y2):max(y1,y2), min(x1,x2):max(x1,x2) ]
+#cv2.rectangle(t, (x1,y1), (x2, y2), (0,255,0), thickness=5)
+#cv2.rectangle(t,(x,y),(x+w,y+h),(0,255,0),2)
+#roi = t[box]
+#print roi.shape
+#cv2.drawContours(t,[cnt],0,(0,0,),-1)
+#rn1 = np.reshape(rn,(17,17,3))
+#print rn1.shape
+cv2.imshow('image', rot_c)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
