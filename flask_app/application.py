027af5239483ad9f54644782259ba34e8c40b38e
Peter Mitrano <mitranopeter@gmail.com>
2016-10-08 16:00:13 -0400

    handle POST of image. saves to disc as img.jpg

--- flask_app/application.py
+++ flask_app/application.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
-    return "hello world"
+    return "hello world\n"
+
+@app.route('/', methods=['POST'])
+def post_img():
+    data = request.files['media']
+    data.save('img.jpg')
+    return 'image uploaded!\n'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
