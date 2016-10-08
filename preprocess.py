c10474a2f4433a7626e1f69d50206bed77f98a2a
Peter Mitrano <mitranopeter@gmail.com>
2016-10-08 12:29:27 -0400

    it works. image is cropped and saved in roi

--- preprocess.py
+++ preprocess.py
@@ -10,21 +10,20 @@ gray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
 contours, hierarchy = cv2.findContours(im_bw.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 t = res.copy()
-#cv2.drawContours(t, contours, 2, (0,255,0), 3)
 cnt = contours[2]
 epsilon = 0.1*cv2.arcLength(cnt,True)
 approx = cv2.approxPolyDP(cnt,epsilon,True)
-print approx
 
 x1 = approx[0][0][0]
 y1 = approx[0][0][1]
 x2 = approx[2][0][0]
 y2 = approx[2][0][1]
 
-cv2.rectangle(t, (x1,y1), (x2, y2), (0,255,0), thickness=10);
+#cv2.rectangle(t, (x1,y1), (x2, y2), (0,255,0), thickness=5);
 
+roi = t[min(y1,y2):max(y1,y2), min(x1,x2):max(x1,x2)]
 
-cv2.imshow('image', t)
+cv2.imshow('image', roi)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
